8eb9f5c0efc663fa69eecb1eacfd1d3a3c9dafbd
thusharakart <thusharakart@gmail.com>
2021-05-15 23:43:31 +0530

    contact details updated

--- scripts/index.js
+++ scripts/index.js
@@ -182,7 +182,7 @@ $(function() {
         e.preventDefault();
 
         $.ajax({
-            url: 'https://formspree.io/mattwilliams85@gmail.com',
+            url: 'https://formspree.io/thusharakart@gmail.com',
             method: 'POST',
             data: { message: $('form').serialize() },
             dataType: 'json'
